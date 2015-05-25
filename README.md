353e66ff186469e70195483961a63202bd86b913
Michael Amore <michael.amore@yahoo.com>
2015-05-25 08:36:33 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# recipes
+This is a website that will have all of our family recipes listed with pictures, ingredient lists, and cooking directions.
+
+
+To-Do:
+	
+
+
+Completed:
---
+++ js/model.js
@@ -0,0 +1,137 @@
+var model = [
+	{
+		"num": 1,
+		"roman_num": "I",
+		"description": "Prohibits the making of any law respecting an establishment of religion, impeding the free exercise of religion, abridging the freedom of speech, infringing on the freedom of the press, interfering with the right to peaceably assemble or prohibiting the petitioning for a governmental redress of grievances."
+	},
+	{
+		"num": 2,
+		"roman_num": "II",
+		"description": "Protects the right to keep and bear arms."
+	},
+	{
+		"num": 3,
+		"roman_num": "III",
+		"description": "Places restrictions on the quartering of soldiers in private homes without the owner's consent, prohibiting it during peacetime."
+	},
+	{
+		"num": 4,
+		"roman_num": "IV",
+		"description": "Prohibits unreasonable searches and seizures and sets out requirements for search warrants based on probable cause as determined by a neutral judge or magistrate."
+	},
+	{
+		"num": 5,
+		"roman_num": "V",
+		"description": "Sets out rules for indictment by grand jury and eminent domain, protects the right to due process, and prohibits self-incrimination and double jeopardy."
+	},
+	{
+		"num": 6,
+		"roman_num": "VI",
+		"description": "Protects the right to a fair and speedy public trial by jury, including the rights to be notified of the accusations, to confront the accuser, to obtain witnesses and to retain counsel."
+	},
+	{
+		"num": 7,
+		"roman_num": "VII",
+		"description": "Provides for the right to trial by jury in certain civil cases, according to common law."
+	},
+	{
+		"num": 8,
+		"roman_num": "VIII",
+		"description": "Prohibits excessive fines and excessive bail, as well as cruel and unusual punishment."
+	},
+	{
+		"num": 9,
+		"roman_num": "IX",
+		"description": "Protects rights not enumerated in the Constitution."
+	},
+	{
+		"num": 10,
+		"roman_num": "X",
+		"description": "Reinforces the principle of federalism by stating that the federal government possesses only those powers delegated to it by the states or the people through the Constitution."
+	},
+	{
+		"num": 11,
+		"roman_num": "XI",
+		"description": "Makes states immune from suits from out-of-state citizens and foreigners not living within the state borders; lays the foundation for sovereign immunity."
+	},
+	{
+		"num": 12,
+		"roman_num": "XII",
+		"description": "Revises presidential election procedures."
+	},
+	{
+		"num": 13,
+		"roman_num": "XIII",
+		"description": "Abolishes slavery, and involuntary servitude, except as punishment for a crime."
+	},
+	{
+		"num": 14,
+		"roman_num": "XIV",
+		"description": "Defines citizenship, contains the Privileges or Immunities Clause, the Due Process Clause, the Equal Protection Clause, and deals with post-Civil War issues."
+	},
+	{
+		"num": 15,
+		"roman_num": "XV",
+		"description": "Prohibits the denial of the right to vote based on race, color, or previous condition of servitude."
+	},
+	{
+		"num": 16,
+		"roman_num": "XVI",
+		"description": "Permits Congress to levy an income tax without apportioning it among the states or basing it on the United States Census."
+	},
+	{
+		"num": 17,
+		"roman_num": "XVII",
+		"description": "Establishes the direct election of United States Senators by popular vote."
+	},
+	{
+		"num": 18,
+		"roman_num": "XVIII",
+		"description": "Prohibited the manufacturing or sale of alcohol within the United States."
+	},
+	{
+		"num": 19,
+		"roman_num": "XIX",
+		"description": "Prohibits the denial of the right to vote based on sex."
+	},
+	{
+		"num": 20,
+		"roman_num": "XX",
+		"description": "Changes the date on which the terms of the President and Vice President (January 20) and Senators and Representatives (January 3) end and begin."
+	},
+	{
+		"num": 21,
+		"roman_num": "XXI",
+		"description": "Repeals the 18th Amendment and prohibits the transportation or importation into the United States of alcohol for delivery or use in violation of applicable laws."
+	},
+	{
+		"num": 22,
+		"roman_num": "XXII",
+		"description": "Limits the number of times that a person can be elected president: a person cannot be elected president more than twice, and a person who has served more than two years of a term to which someone else was elected cannot be elected more than once."
+	},
+	{
+		"num": 23,
+		"roman_num": "XXIII",
+		"description": "Grants the District of Columbia electors (the number of electors being equal to the least populous state) in the Electoral College."
+	},
+	{
+		"num": 24,
+		"roman_num": "XXIV",
+		"description": "Prohibits the revocation of voting rights due to the non-payment of a poll tax."
+	},
+	{
+		"num": 25,
+		"roman_num": "XXV",
+		"description": "Addresses succession to the Presidency and establishes procedures both for filling a vacancy in the office of the Vice President, as well as responding to Presidential disabilities."
+	},
+	{
+		"num": 26,
+		"roman_num": "XXVI",
+		"description": "Prohibits the denial of the right of US citizens, eighteen years of age or older, to vote on account of age."
+	},
+	{
+		"num": 27,
+		"roman_num": "XXVII",
+		"description": "Delays laws affecting Congressional salary from taking effect until after the next election of representatives."
+	},
+];
